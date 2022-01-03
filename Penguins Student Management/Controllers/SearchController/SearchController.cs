e2502f84955317477ecdc68d2611db4c0b5981db
Nguyễn Thanh Hoài <thanhhoai5683@gmail.com>
2022-01-03 09:30:47 +0700

    Add SearchController

---
+++ Penguins Student Management/Controllers/SearchController/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using Penguins_Student_Management.JsonDatabase.Entity.Document;
+using Penguins_Student_Management.StateManagement.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Penguins_Student_Management.Controllers.SearchController
+{
+    partial class SearchController : IRiverSubject {
+
+        public List<User> Users;
+        public List<Class> Classes;
+        public List<Course> Courses;
+
+        public SearchController()
+        {
+            Users = new List<User>();
+            Classes = new List<Class>();
+            Courses = new List<Course>();
+        }
+    
+    }
+}
