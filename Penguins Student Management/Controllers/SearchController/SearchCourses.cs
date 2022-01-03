99282b73894a275448390089132f88bebe72138a
Nguyễn Thanh Hoài <thanhhoai5683@gmail.com>
2022-01-03 09:51:12 +0700

    Add SearchCourses func

---
+++ Penguins Student Management/Controllers/SearchController/SearchCourses.cs
@@ -0,0 +1,30 @@
+﻿using Penguins_Student_Management.JsonDatabase.Entity.Document;
+using Penguins_Student_Management.StateManagement.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Penguins_Student_Management.Controllers.SearchController
+{
+    partial class SearchController {
+
+        public List<Course> SearchCourses(string SearchQuery)
+        {
+            List<Course> courses = new List<Course>();
+
+            Global.Database.Collections.Courses.Values.ToList().ForEach(course => {
+
+                if(course.Name.Contains(SearchQuery) || course.Owner.Contains(SearchQuery))
+                {
+                    courses.Add(course);
+                }
+            
+            });
+
+            return courses;
+        }
+    
+    }
+}
